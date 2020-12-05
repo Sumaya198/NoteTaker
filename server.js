ece541f42be2e8e987fca5e919ff42282bf4dae4
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2020-12-05 18:27:36 +0000

    initiating the server

--- server.js
+++ server.js
@@ -1,5 +1,44 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const PORT = process.env.PORT || 3035;
+
+let noteApp = express();
+
+noteApp.use(express.urlencoded({ extended: true}));
+noteApp.use(express.json());
+noteApp.use(express.static("./public"));
+
+// noteApp.use('/public/assets/js/index', express.static(path.resolve(__dirname, 'public/assets/js/index')));
+
+//routing the code to map the requests from simple handlers depending on the url
+noteApp.get("/notes", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/public/notes.html"))
+})
+
+noteApp.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/public/index.html"))
+})
+
+noteApp.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/public/index.html"))
+})
+
+noteApp.get("api/notes", (req,res) => {
+   
+})
+
+noteApp.post("/api/notes", (req,res) => {
+   
+});
+
+noteApp.delete("/api/notes/:id", (req, res) => {
+   
+})
+
+noteApp.listen(PORT, function(){
+    console.log("listenting " + PORT);
+})
 
 
 
