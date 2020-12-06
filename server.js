ac6cb2432a5417fcf6923152e25a4fb56c1d0cc9
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2020-12-06 22:32:14 +0000

    updated post and get request

--- server.js
+++ server.js
@@ -1,47 +1,83 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const PORT = process.env.PORT || 3035;
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5000;
 
-let noteApp = express();
+let app = express();
 
-noteApp.use(express.urlencoded({ extended: true}));
-noteApp.use(express.json());
-noteApp.use(express.static("./public"));
+//Server set up and static files
+//bodyParser.urlencoded({extended: false})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-// noteApp.use('/public/assets/js/index', express.static(path.resolve(__dirname, 'public/assets/js/index')));
+const readFileAsynchrously = util.promisify(fs.readFile);
+const writeFileAsynchronously = util.promisify(fs.writeFile);
+let notes = [];
 
-//routing the code to map the requests from simple handlers depending on the url
-noteApp.get("/notes", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/public/notes.html"))
-})
+// // app.use('/public/assets/js/index', express.static(path.resolve(__dirname, 'public/assets/js/index')));
 
-noteApp.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/public/index.html"))
-})
 
-noteApp.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/public/index.html"))
-})
 
-noteApp.get("api/notes", (req,res) => {
-   
-})
-
-noteApp.post("/api/notes", (req,res) => {
-   
+function readingTheDbFile() {
+//   fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function (data) {
+//     return JSON.parse(data);
+  
+  return readFileAsynchrously(path.join(__dirname, "/db/db.json"), "utf8");
+}
+// Get request/ route. The aim is to read the db.json file and the return it
+//url path we are going to, to ask for information
+//take in information and give you back more information
+app.get("/api/notes", (req, res) => {
+  //to convert the notes from json to javascript object
+   readingTheDbFile().then((data) => {
+    notes = data;
+    console.log("get request", data);
+    //will return object to the ui
+    res.json(JSON.parse(data));
+  });
+  
 });
 
-noteApp.delete("/api/notes/:id", (req, res) => {
-   
-})
+//main aim is to take info in and send information back
+//POST request/ route. The aim is to read the db.json file but
+app.post("/api/notes", (req, res) => {
+  //function to read the json file
+  readingTheDbFile();
+  // you have to use an id?..
+  //what is the id??? what does it do??
+  let id;
+  let newNotes = req.body;
+  notes.push(newNotes);
+  console.log(JSON.stringify(notes), "pushed into db json file");
+  fs.writeFile(
+    path.join(__dirname, "/db/db.json"),
+    JSON.stringify(notes),
+    function (data) {
+      return res.json(notes);
+    }
+  );
+});
 
-noteApp.listen(PORT, function(){
-    console.log("listenting " + PORT);
-})
+app.delete("/api/notes/:id", function (req, res) {
+  fs.readFile(path.join(__dirname, "/db/db.json"), "utf8");
+});
 
+// //routing the code to map the requests from simple handlers depending on the url
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+  
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+  
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "/public/index.html"));
+  // });
 
-
-
-
-
+app.listen(PORT, function () {
+  console.log("Listening at port ", +PORT);
+});
